51804bcc309f65efa4a0abf76d942b256dd38e16
noceg43 <noceg43@gmail.com>
2025-01-04 17:51:39 +0100

    add: 3.2, 3.3, 3.4, 3.5 & 3.6

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // do not use ES module it's not suggested in this course
 const express = require('express')
 
-const persons = require('./persons.json');
+const fs = require('fs')
+
+const filename = './persons.json'
+
+const persons = require(filename);
 
 const app = express()
 
@@ -13,6 +17,71 @@ app.use(express.json())
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(note => note.id === id)
+
+    // return 404 if note is not found
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+const updatePersons = (newContent) => fs.writeFileSync(filename, JSON.stringify(newContent))
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(note => note.id === id)
+
+    // return 404 if note is not found
+    if (person) {
+        newContent = persons.filter(person => person.id !== id)
+        updatePersons(newContent)
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    // if content is missing, return 400
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if (persons.find(person => body.name == person.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+     }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    updatePersons(persons.concat(person))
+
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+    response.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + new Date())
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
