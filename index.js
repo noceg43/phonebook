0ff52987ef996caeae6318df990239390d96feb9
noceg43 <noceg43@gmail.com>
2025-01-04 17:24:04 +0100

    add: 3.1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const http = require('http')
+// do not use ES module it's not suggested in this course
+const express = require('express')
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const persons = require('./persons.json');
+
+const app = express()
+
+// activate json parser
+app.use(express.json())
+
+// routes
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
