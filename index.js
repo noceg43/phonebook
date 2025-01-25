1ea1876ac3ef123dca658538dbbfff04b613eec4
noceg43 <noceg43@gmail.com>
2025-01-25 17:47:10 +0100

    add: 3.17, 3.18

--- index.js
+++ index.js
@@ -48,24 +48,25 @@ app.use(morgan(':method :url :status :total-time[4] ms :body'))
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
-      })})
+    })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findById(id).then(
         person => {
-    // return 404 if note is not found
-    if (person) {
-        response.json(person)
-    } else {
-        console.log(error)
-       // 404 Not Found, since the note doesn't exist
-       response.status(404).end()
-    }        
-    }
-    )    
-    // Pass to the middleware error handler
-    .catch(error => next(error))
+            // return 404 if note is not found
+            if (person) {
+                response.json(person)
+            } else {
+                console.log(error)
+                // 404 Not Found, since the note doesn't exist
+                response.status(404).end()
+            }
+        }
+    )
+        // Pass to the middleware error handler
+        .catch(error => next(error))
 })
 
 
@@ -76,7 +77,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(id).then(result => {
         response.status(204).end()
     })
-    .catch(error => next(error))
+        .catch(error => next(error))
 })
 
 
@@ -87,55 +88,55 @@ app.post('/api/persons', (request, response, next) => {
             error: 'content missing'
         })
     }
-    Person.find({name: body.name}).then(
+    Person.findOne({ name: body.name }).then(
         person => {
-    // return 404 if note is not found
-    if (person) {
-        const newData = { name: body.name, number: body.number }
-        Person.findByIdAndUpdate(person.id, newData, { new: true }).then(updatedPerson => {
-            response.json(updatedPerson)
-        }).catch(error => next(error))
-     
-    } else {
-        const person =  new Person({ name: body.name, number: body.number })
+            // return 404 if note is not found
+            if (person) {
+                const newData = { name: body.name, number: body.number }
+                Person.findByIdAndUpdate(person._id, newData, { new: true }).then(updatedPerson => {
+                    response.json(updatedPerson)
+                }).catch(error => next(error))
 
-        person.save().then(savedPerson => {
-        response.json(savedPerson)
-        })
-    }        
-    }
-)    
+            } else {
+                const person = new Person({ name: body.name, number: body.number })
 
-
-
+                person.save().then(savedPerson => {
+                    response.json(savedPerson)
+                })
+            }
+        }
+    )
 })
 
 app.get('/info', (request, response) => {
-    response.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + new Date())
+
+    Person.find({}).then(persons => {
+        response.send('<p>Phonebook has info for ' + persons.length + ' people</p>' + new Date())
+    })
 })
 
 
 // Catch-all middleware for non-existent routes (404)
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
-  };
+};
 app.use(unknownEndpoint);
 
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
+
     // throwed if the error is a validation error (for the GET request with an invalid id)
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
     next(error)
-  }
-  
+}
+
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler)
-  
+
 
 
 const PORT = process.env.PORT || 3001
